b6b5c99f865ec3a5a554a459e9ff9cf256247a06
Ankit Kadyan <ankit.kadyan@designx.in>
2023-10-16 15:51:13 +0530

    Timeline.js

--- src/Components/AboutUs/Timeline.js
+++ src/Components/AboutUs/Timeline.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Timeline = () => {
   return (
-    <div className='mt-[120px]'>
-    <div className='bg-[#CAE8FF] w-full h-[687px] pt-[30px] pl-[100px]'>
-      <p className='text-[#1B1A20] text-[42px] font-robotoSlab'>DesignX Timeline</p>
-    </div>
-    <div className='requestDemo w-[80%] h-[137px] mx-[10%] absolute top-[3090px]'>
-        <div className='mt-[30px] lg:mt-[46px] ml-[30px] lg:ml-[75px]'>
-          <p className='text-[24px] sm:text-[30px] xl:text-[42px] text-[#222F66] font-robotoSlab'>DFOS – Your unfair advantage in the digital world</p>
-          
+    <div className="mt-[120px]">
+      <div className="bg-[#CAE8FF] w-full h-[687px] pt-[30px] pl-[100px]">
+        <p className="text-[#1B1A20] text-[42px] font-robotoSlab">
+          DesignX Timeline
+        </p>
+        {/* carousel */}
+      </div>
+      <div className="requestDemo w-[80%] h-[137px] mx-[10%] absolute top-[3090px]">
+        <div className="mt-[30px]">
+          <p className="text-[24px] sm:text-[30px] xl:text-[42px] text-center text-[#222F66] font-robotoSlab">
+            DFOS - Your unfair advantage in the digital world
+          </p>
         </div>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Timeline
+export default Timeline;
--- src/Components/AboutUs/Vision.js
+++ src/Components/AboutUs/Vision.js
@@ -6,23 +6,23 @@ import poster from '../assets/images/poster.svg'
 
 const Vision = () => {
   return (
-    <div className='mt-[147px] bg-[#EFF6FF] w-full h-[232px] px-[85px] py-[57px] mb-[50px]'>
+    <div className='mt-[147px] bg-[#EFF6FF] w-full h-[500px] lg:h-[232px] px-[85px] py-[57px] mb-[50px]'>
       <div className='flex'>
-        <div className='grid grid-cols-3 gap-4'>
-            <div className='hoverBtn flex pl-[31px] py-[22px]'>
+        <div className='block lg:grid lg:grid-cols-3 gap-4'>
+            <div className='hoverBtn flex ml-[70%] lg:ml-0 pl-[31px] py-[22px]'>
               <img src={mission} alt="" className='w-[78px] h-[78px]' />
               <p className='text-white text-[30px] font-poppins ml-[20px] mt-[15px]'>Mission</p>
             </div>
-            <div className='hoverBtn flex ml-[20px] pl-[31px] py-[22px]'>
+            <div className='hoverBtn flex ml-[70%] lg:ml-[20px] mt-[20px] lg:mt-0 pl-[31px] py-[22px]'>
               <img src={vision} alt="" className='w-[78px] h-[78px]' />
               <p className='text-white text-[30px] font-poppins ml-[20px] mt-[15px]'>Vision</p>
             </div>
-            <div className='hoverBtn flex ml-[30px] pl-[31px] py-[15px]'>
+            <div className='hoverBtn flex ml-[70%] lg:ml-[30px] mt-[20px] lg:mt-0 pl-[31px] py-[15px]'>
               <img src={values} alt="" className='w-[78px] h-[78px]' />
               <p className='text-white text-[30px] font-poppins ml-[20px] mt-[20px]'>Values</p>
             </div>
         </div>
-        <div className='circleDot'>
+        <div className='circleDot hidden lg:block'>
             <img src={poster} alt="" className='relative left-[56px] bottom-[90px] w-[213px] h-[357px]' />
         </div>
       </div>
